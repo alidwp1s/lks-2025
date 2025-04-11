2527d913f1f157059a3cb88d75213afe851ac96c
Ali Dwi Prastiyo <alidwiprasetyo05@gmail.com>
2025-04-11 14:36:37 +0700

    modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\GameController;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
